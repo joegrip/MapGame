946925b4770fbb33aa41435e2b9ff5925797cb24
Joseph Gripenstraw <jgripens@nd.edu>
2020-04-28 15:04:39 -0500

    Initial Commit

---
+++ MapGame/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  MapGame
+//
+//  Created by Joseph Gripenstraw on 4/28/20.
+//  Copyright © 2020 GripenstrawLLC. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
